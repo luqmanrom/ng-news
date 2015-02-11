40f8a7f92b3ec50530fd5c93a5415cd1a6903698
LuqmanRom <moham601@umn.edu>
2015-02-11 03:20:38 -0600

    Back to original

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -4,23 +4,9 @@
 
 'use strict';
 
-app.factory('Post', function ($firebase,FIREBASE_URL) {
-    var ref = new Firebase(FIREBASE_URL);
-    var posts = $firebase(ref.child('posts')).$asArray();
+app.factory('Post', function ($resource) {
+    return $resource('https://blinding-torch-4455.firebaseio.com/posts/:id.json');
 
-    var Post = {
-        all: posts,
-        create: function (post) {
-            return posts.$add(post);
-        },
-        get: function (postId) {
-            return $firebase(ref.child('posts').child(postId)).$asObject();
-        },
-        delete: function (post) {
-            return posts.$remove(post);
-        }
-    };
 
-    return Post;
 
 });
