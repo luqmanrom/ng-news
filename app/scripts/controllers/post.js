bca65f98adb5239ec75703da2ee68e582ffaccd8
LuqmanRom <moham601@umn.edu>
2015-02-11 03:48:28 -0600

    Using three way data binding

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -5,28 +5,19 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope,Post) {
-    $scope.posts = Post.get();
+    $scope.posts = Post.all;
     $scope.post = {url: 'http:///', title: ''};
 
 
     $scope.submitPost = function () {
 
-        Post.save($scope.post, function (ref) {
-            $scope.posts[ref.name] = $scope.post;
-            console.log(ref.name);
-            $scope.post = {url: 'http://', title: ''};
+        Post.create($scope.post).then(function () {
+            $scope.post = {url: 'http://', 'title': ''};
         });
-
-        
-        $scope.posts.push($scope.post);
-        $scope.post = {url: 'http://', title: ''};
     };
 
-    $scope.deletePost = function(postId) {
-        //$scope.posts.splice(index,1);
-        Post.delete({id: postId}, function () {
-            delete $scope.posts[postId];
-        });
+    $scope.deletePost =  function (post) {
+        Post.delete(post);
     };
 
 
